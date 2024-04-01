aecbc1b9f413cbe6741fb8b1edadb101b73fe9a7
VladissGernet <skeimorr@gmail.com>
2024-04-01 16:55:39 +0400

    js swiper files added

--- source/js/main.js
+++ source/js/main.js
@@ -3,10 +3,33 @@
 // import {Navigation, Pagination} from "swiper/modules";
 // import 'swiper/css';
 
-import { initAboutVideo } from './about-video.js';
-import { initPriceFilter } from './price-filter.js';
+import { initAboutVideo } from './modules/about-video.js';
+import { initPriceFilter } from './modules/price-filter.js';
+import { Swiper } from 'swiper';
 
 initAboutVideo();
 initPriceFilter();
 
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'vertical',
+  loop: true,
 
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
+
+console.log(swiper);
