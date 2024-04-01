9d79485e0f484a07e0a422b0f1205f768fc459b8
VladissGernet <skeimorr@gmail.com>
2024-04-01 19:15:07 +0400

    swiper is connected

--- source/js/main.js
+++ source/js/main.js
@@ -5,13 +5,17 @@
 
 import { initAboutVideo } from './modules/about-video.js';
 import { initPriceFilter } from './modules/price-filter.js';
-import { Swiper } from 'swiper';
 
 initAboutVideo();
 initPriceFilter();
 
+// import { Swiper } from './vendor/swiper.js';
 
-const swiper = new Swiper('.swiper-juri', {
+new Swiper('.swiper-juri', {
   loop: true,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
 });
 
