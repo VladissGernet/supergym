4efb138dedc77d4e5bcb61153e4193899f1827d7
VladissGernet <skeimorr@gmail.com>
2024-04-02 11:08:14 +0400

    juri checkout

--- source/js/main.js
+++ source/js/main.js
@@ -1,8 +1,3 @@
-// https://swiperjs.com/get-started#installation
-// import Swiper from "swiper";
-// import { Navigation, Pagination } from "swiper/modules";
-// import 'swiper/css';
-
 import { initAboutVideo } from './modules/about-video.js';
 import { initPriceFilter } from './modules/price-filter.js';
 
@@ -11,11 +6,28 @@ initPriceFilter();
 
 import { Swiper } from './vendor/swiper.js';
 
-new Swiper('.swiper-juri', {
+const swiperJuri = new Swiper('.swiper-juri', {
   loop: true,
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
   },
+});
+
+const TABLET_WIDTH = 768;
+const DESKTOP_WIDTH = 1366;
+
+window.addEventListener('resize', () => {
+  switch (true) {
+    case window.innerWidth < TABLET_WIDTH:
+      swiperJuri.params.slidesPerView = 1;
+      break;
+    case window.innerWidth >= TABLET_WIDTH && window.innerWidth < DESKTOP_WIDTH:
+      swiperJuri.params.slidesPerView = 2;
+      break;
+    case window.innerWidth >= DESKTOP_WIDTH:
+      swiperJuri.params.slidesPerView = 4;
+  }
+  swiperJuri.update();
 });
 
