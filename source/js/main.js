3b5ad4a124ef7929a75a4948d8fcd14288bb6e4a
VladissGernet <skeimorr@gmail.com>
2024-04-02 19:37:02 +0400

    faq checkout

--- source/js/main.js
+++ source/js/main.js
@@ -7,3 +7,30 @@ initAboutVideo();
 initPriceFilter();
 initSwiperJuri();
 initSwiperReviews();
+
+const faqSection = document.querySelector('.faq');
+const faqList = faqSection.querySelector('.faq__list');
+const faqAccordions = faqSection.querySelectorAll('.faq__accordion');
+
+
+faqList.addEventListener('click', (evt) => {
+  const selectedButton = evt.target;
+  if (selectedButton.classList.contains('faq__button') === false) {
+    return;
+  }
+  const previousActiveButton = faqList.querySelector('.tab-button--active');
+  previousActiveButton.classList.remove('tab-button--active');
+  const targetButton = selectedButton.closest('.faq__button');
+  targetButton.classList.add('tab-button--active');
+
+  for (const accordion of faqAccordions) {
+    const isAccordionNotHid = accordion.classList.contains('hidden') === false;
+    if (isAccordionNotHid) {
+      accordion.classList.add('hidden');
+    }
+    if (accordion.dataset.faqAccordion === targetButton.dataset.faqButton) {
+      accordion.classList.remove('hidden');
+    }
+  }
+});
+
